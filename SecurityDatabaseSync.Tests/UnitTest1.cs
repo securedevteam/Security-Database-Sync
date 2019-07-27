e11e2f2a812dd22d249be1520d6dcdfcfd9629b7
Михаил Масный <masny.mikhail@gmail.com>
2019-07-27 18:28:32 +0300

    Добавлен проект *.Tests. Добавлены необходимые ссылки.

---
+++ SecurityDatabaseSync.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecurityDatabaseSync.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

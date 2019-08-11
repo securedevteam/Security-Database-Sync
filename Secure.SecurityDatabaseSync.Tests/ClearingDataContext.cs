edde6a9c05bae800d40e74a2c9def1069ae8bb42
Nishikava <42912978+Nishikava@users.noreply.github.com>
2019-08-11 21:40:47 +0300

    Реализован InMemory.

---
+++ Secure.SecurityDatabaseSync.Tests/ClearingDataContext.cs
@@ -0,0 +1,27 @@
+﻿using SecurityDatabaseSync.DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Secure.SecurityDatabaseSync.Tests
+{
+    class ClearingDataContext
+    {
+        private readonly ApplicationContext _context;
+
+        public ClearingDataContext(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public void Clear()
+        {
+            foreach (var entity in _context.TestModelTable)
+            {
+                _context.TestModelTable.Remove(entity);
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}

62b37afd4b3140732d90b50f901a5904bd9e633f
Nishikava <42912978+Nishikava@users.noreply.github.com>
2019-08-12 06:28:14 +0300

    Добавлена базовая структура тестов BulkHardSyncControllerTests и DefaultSyncControllerTests.

--- Secure.SecurityDatabaseSync.Tests/BulkHardSyncControllerTests.cs
+++ Secure.SecurityDatabaseSync.Tests/BulkHardSyncControllerTests.cs
@@ -1,9 +1,48 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SecurityDatabaseSync.DAL;
+using System;
 using Xunit;
 
 namespace Secure.SecurityDatabaseSync.Tests
 {
-    public class BulkHardSyncControllerTests
+    public class BulkHardSyncControllerTests : IClassFixture<ServiceFixture>, IDisposable
     {
+        private readonly ServiceProvider _serviceProvider;
+        private readonly ApplicationContext _context;
+        private readonly ClearingDataContext _clearingDataContext;
+
+        private readonly Random rnd = new Random();
+
+        public BulkHardSyncControllerTests(ServiceFixture fixture)
+        {
+            _serviceProvider = fixture.ServiceProvider;
+
+            _context = _serviceProvider.GetRequiredService<ApplicationContext>();
+
+            _clearingDataContext = new ClearingDataContext(_context);
+        }
+
+        public void Dispose()
+        {
+            _clearingDataContext.Clear();
+        }
+
+        [Fact]
+        public void InsertDataAsync_Return_True()
+        {
+
+        }
+
+        [Fact]
+        public void ClearDataAsync_Return_True()
+        {
+
+        }
+
+        [Fact]
+        public void CopyDataAsync_Return_True()
+        {
+
+        }
     }
 }
--- Secure.SecurityDatabaseSync.Tests/DefaultSyncControllerTests.cs
+++ Secure.SecurityDatabaseSync.Tests/DefaultSyncControllerTests.cs
@@ -1,9 +1,48 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SecurityDatabaseSync.DAL;
+using System;
 using Xunit;
 
 namespace Secure.SecurityDatabaseSync.Tests
 {
-    public class DefaultSyncControllerTests
+    public class DefaultSyncControllerTests : IClassFixture<ServiceFixture>, IDisposable
     {
+        private readonly ServiceProvider _serviceProvider;
+        private readonly ApplicationContext _context;
+        private readonly ClearingDataContext _clearingDataContext;
+
+        private readonly Random rnd = new Random();
+
+        public DefaultSyncControllerTests(ServiceFixture fixture)
+        {
+            _serviceProvider = fixture.ServiceProvider;
+
+            _context = _serviceProvider.GetRequiredService<ApplicationContext>();
+
+            _clearingDataContext = new ClearingDataContext(_context);
+        }
+
+        public void Dispose()
+        {
+            _clearingDataContext.Clear();
+        }
+
+        [Fact]
+        public void GetDataFromDatabaseAsync_Return_True()
+        {
+
+        }
+
+        [Fact]
+        public void AddOrDeleteDataToDatabaseAsync_Return_True()
+        {
+
+        }
+
+        [Fact]
+        public void UpdateDataToServerAsync_Return_True()
+        {
+
+        }
     }
 }

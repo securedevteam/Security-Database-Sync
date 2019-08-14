4a4f2febe9876505dc68644f785441e416be4789
Михаил Масный <masny.mikhail@gmail.com>
2019-08-14 21:08:11 +0300

    Реализованы тесты для BulkHardSyncController.

--- SecurityDatabaseSync.Tests.xUnit/BulkHardSyncControllerTests.cs
+++ SecurityDatabaseSync.Tests.xUnit/BulkHardSyncControllerTests.cs
@@ -12,57 +12,50 @@ namespace Secure.SecurityDatabaseSync.Tests
     public class BulkHardSyncControllerTests
     {
         private readonly Random rnd = new Random();
+        private readonly string _databaseFirstName = Guid.NewGuid().ToString();
+        private readonly string _databaseSecondName = Guid.NewGuid().ToString();
+        private readonly string _identifier = "test_";
 
         [Fact]
         public async void InsertDataAsync_Return_True()
         {
             // Arrange
-            var databaseName = Guid.NewGuid().ToString();
-            var identifier = $"{rnd.Next(100).ToString()} ";
             var operationResult = true;
 
             // Act
             ISyncController syncController = new BulkHardSyncController();
-
-            var result = await syncController.InsertDataAsync(databaseName, identifier);
+            var resultFirst = await syncController.InsertDataAsync(_databaseFirstName, _identifier);
+            var resultSecond = await syncController.InsertDataAsync(_databaseSecondName, _identifier);
 
             // Assert
+            Assert.Equal(operationResult, resultFirst);
+            Assert.Equal(operationResult, resultSecond);
+        }
+
+        [Fact]
+        public async void ClearDataWithIdentifierAsync_Return_False()
+        {
+            // Arrange
+            var operationResult = false;
+            
+            // Act
+            ISyncController syncController = new BulkHardSyncController();
+            var result = await syncController.ClearDataAsync(_databaseFirstName, string.Empty);
+
             Assert.Equal(operationResult, result);
         }
 
         [Fact]
-        public async void ClearDataAsync_Return_True()
+        public async void CopyDataAsync_Return_True()
         {
-            var databaseName = "DataBase";
-            var identifier = "1";
-            var listTestModel = new List<TestModel>();
-            var expected = 10;
+            // Arrange
             var operationResult = true;
 
+            // Act
             ISyncController syncController = new BulkHardSyncController();
-
-            for (int i = 0; i < expected; i++)
-            {
-                listTestModel.Add(new TestModel
-                {
-                    Code = identifier + Guid.NewGuid().ToString(),
-                    Name = Guid.NewGuid().ToString().Substring(0, 8),
-                    Current = DateTime.Now
-                });
-            }
-
-            //_context.TestModelTable.AddRange(listTestModel);
-            //_context.SaveChanges();
-
-            var result = await syncController.ClearDataAsync(databaseName);
+            var result = await syncController.CopyDataAsync(_databaseFirstName, _databaseSecondName, _identifier);
 
             Assert.Equal(operationResult, result);
-        }
-
-        [Fact]
-        public void CopyDataAsync_Return_True()
-        {
-
         }
     }
 }
--- SecurityDatabaseSync.Tests.xUnit/DefaultSyncControllerTests.cs
+++ SecurityDatabaseSync.Tests.xUnit/DefaultSyncControllerTests.cs
@@ -5,44 +5,8 @@ using Xunit;
 
 namespace Secure.SecurityDatabaseSync.Tests
 {
-    public class DefaultSyncControllerTests : IClassFixture<ServiceFixture>, IDisposable
+    public class DefaultSyncControllerTests
     {
-        private readonly ServiceProvider _serviceProvider;
-        private readonly ApplicationContext _context;
-        private readonly ClearingDataContext _clearingDataContext;
-
-        private readonly Random rnd = new Random();
-
-        public DefaultSyncControllerTests(ServiceFixture fixture)
-        {
-            _serviceProvider = fixture.ServiceProvider;
-
-            _context = _serviceProvider.GetRequiredService<ApplicationContext>();
-
-            _clearingDataContext = new ClearingDataContext(_context);
-        }
-
-        public void Dispose()
-        {
-            _clearingDataContext.Clear();
-        }
-
-        [Fact]
-        public void GetDataFromDatabaseAsync_Return_True()
-        {
-
-        }
-
-        [Fact]
-        public void AddOrDeleteDataToDatabaseAsync_Return_True()
-        {
-
-        }
-
-        [Fact]
-        public void UpdateDataToServerAsync_Return_True()
-        {
-
-        }
+        // TODO: Не реализован
     }
 }

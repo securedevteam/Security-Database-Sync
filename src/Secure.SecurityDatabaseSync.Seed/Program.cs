1935ba08fcb54fbc5c1b2020a0b2c0500537212c
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2021-03-13 16:06:35 +0300

    feat: add implementation of simple database seed application

---
+++ src/Secure.SecurityDatabaseSync.Seed/Program.cs
@@ -0,0 +1,41 @@
+﻿using Secure.SecurityDatabaseSync.DAL.Contexts;
+using Secure.SecurityDatabaseSync.DAL.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Secure.SecurityDatabaseSync.Seed
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Database filling started..");
+
+            IEnumerable<FirstModel> GetFirstModels()
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    yield return new FirstModel
+                    {
+                        Name = Guid.NewGuid().ToString(),
+                        Description = Guid.NewGuid().ToString(),
+                    };
+                }
+            }
+
+            try
+            {
+                using var context = new FirstAppContext();
+                await context.FirstModels.AddRangeAsync(GetFirstModels());
+                await context.SaveChangesAsync();
+
+                Console.WriteLine("Database filling ended..");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Database filling ended with error..");
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
